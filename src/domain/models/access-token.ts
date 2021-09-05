eea59b413d8846f97cd641c6196ac9d478d393a1
Thiago Turim Carvalho <tiago.carvalho@br.experian.com>
2021-09-05 15:29:01 -0300

    ensure AccessToken expiresIn 30 minutes

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,3 +1,7 @@
 export class AccessToken {
   constructor(private readonly value: string) { }
+
+  static get expirationsInMs(): number {
+    return 30 * 60 * 1000;
+  }
 }
--- tests/data/domain/models/access-token.spec.ts
+++ tests/data/domain/models/access-token.spec.ts
@@ -5,4 +5,8 @@ describe('AccessToken', () => {
     const sut = new AccessToken('any_value');
     expect(sut).toEqual({ value: 'any_value' });
   });
+
+  it('should expire in 1800000 ms minutes', () => {
+    expect(AccessToken.expirationsInMs).toBe(1800000);
+  });
 });
