9132a52f141ed2771a0c3e99bc130dfc63f2ae55
Thiago Turim Carvalho <tiago.carvalho@br.experian.com>
2021-09-08 22:37:31 -0300

    isolate sut creation

--- tests/infra/facebook.spec.ts
+++ tests/infra/facebook.spec.ts
@@ -32,12 +32,23 @@ namespace HttpGetClient {
 
 
 describe('FacebookApi', () => {
-  const clientId = 'any_client_id';
-  const clientSecret = 'any_client_secret';
+  let clientId: string;
+  let clientSecret: string;
+  let sut: FacebookApi;
+  let httpClient: HttpGetClient;
+
+  beforeAll(() => {
+    httpClient = mock();
+    clientId = 'any_client_id';
+    clientSecret = 'any_client_secret';
+  });
+
+  beforeEach(() => {
+    sut = new FacebookApi(httpClient, clientId, clientSecret);
+  });
 
   it('should get app token', async () => {
-    const httpClient = mock<HttpGetClient>();
-    const sut = new FacebookApi(httpClient, clientId, clientSecret);
+
     await sut.loadUser({ token: 'any_client_id' });
     expect(httpClient.get).toHaveBeenCalledWith({
       url: 'https://graph.facebook.com/oath/access_token',
