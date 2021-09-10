9163bb8da04a75ba5ca12d381664352ee3d7f206
Thiago Turim Carvalho <tiago.carvalho@br.experian.com>
2021-09-10 08:12:15 -0300

    ensure JwtTokenGenerator retuns token on success

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -6,9 +6,11 @@ jest.mock('jsonwebtoken');
 class JwtTokenGenerator {
   constructor(private readonly secret: string) {}
 
-  async generateToken(params: TockenGenerator.Params): Promise<void> {
+  async generateToken(
+    params: TockenGenerator.Params
+  ): Promise<TockenGenerator.Result> {
     const expirationInSeconds = params.expirationInMs / 1000;
-    jwt.sign({ key: params.key }, this.secret, {
+    return jwt.sign({ key: params.key }, this.secret, {
       expiresIn: expirationInSeconds
     });
   }
@@ -22,6 +24,7 @@ describe('JwtTokenGenerator', () => {
   beforeEach(() => {
     secret = 'any_secret';
     fakeJwt = jwt as jest.Mocked<typeof jwt>;
+    fakeJwt.sign.mockImplementation(() => 'any_token');
     sut = new JwtTokenGenerator(secret);
   });
 
@@ -32,5 +35,13 @@ describe('JwtTokenGenerator', () => {
       'any_secret',
       { expiresIn: 1 }
     );
+  });
+
+  it('should return a token', async () => {
+    const token = await sut.generateToken({
+      key: 'any_key',
+      expirationInMs: 1000
+    });
+    expect(token).toBe('any_token');
   });
 });
