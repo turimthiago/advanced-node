7b65175f99d029d7f2dee154b60dde7fa8b43ed9
Thiago Turim Carvalho <tiago.carvalho@br.experian.com>
2021-09-12 15:33:25 -0300

    move inMemoryDb creation to its own file

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -2,16 +2,7 @@ import { IBackup, IMemoryDb, newDb } from 'pg-mem';
 import { getConnection, getRepository, Repository } from 'typeorm';
 import { PgUser } from '@/infra/postgres/entities';
 import { PgUserAccountRepository } from '@/infra/postgres/repos';
-
-const makeFakedb = async (entities?: any[]): Promise<IMemoryDb> => {
-  const db = newDb();
-  const connection = await db.adapters.createTypeormConnection({
-    type: 'postgres',
-    entities: entities ?? ['src/infra/postgres/entities/index.ts']
-  });
-  await connection.synchronize();
-  return db;
-};
+import { makeFakedb } from '@/tests/infra/postgres/mocks';
 
 describe('PgUserAccountRepository', () => {
   let sut: PgUserAccountRepository;
@@ -36,7 +27,6 @@ describe('PgUserAccountRepository', () => {
   describe('load', () => {
     it('should return an account if email exists', async () => {
       await pgUserRepository.save({ email: 'exists@mail.com' });
-      const sut = new PgUserAccountRepository();
       const account = await sut.load({ email: 'exists@mail.com' });
       expect(account).toEqual({ id: '1' });
     });
