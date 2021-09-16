f63ecda87950d9bc61b15e80799d77e319e2e4cb
Thiago Turim Carvalho <tiago.carvalho@br.experian.com>
2021-09-16 07:38:00 -0300

    extract validation logic to a method

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -10,7 +10,7 @@ import { AccessToken } from '@/domain/models';
 import { RequiredFieldError, UnknownError } from '@/application/errors';
 
 type HttpRequest = {
-  token: string | undefined | null;
+  token: string;
 };
 
 type Model =
@@ -26,12 +26,9 @@ export class FacebookLoginController {
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     try {
-      if (
-        httpRequest.token === '' ||
-        httpRequest.token === null ||
-        httpRequest.token === undefined
-      ) {
-        return badRequest(new RequiredFieldError('token'));
+      const error = this.validate(httpRequest);
+      if (error !== undefined) {
+        return badRequest(error);
       }
       const accessToken = await this.facebookAuthentication.perform({
         token: httpRequest.token
@@ -43,6 +40,16 @@ export class FacebookLoginController {
     } catch (error) {
       if (error instanceof Error) return serverError(error);
       return serverError(new UnknownError());
+    }
+  }
+
+  private validate(httpRequest: HttpRequest): Error | undefined {
+    if (
+      httpRequest.token === '' ||
+      httpRequest.token === null ||
+      httpRequest.token === undefined
+    ) {
+      return new RequiredFieldError('token');
     }
   }
 }
