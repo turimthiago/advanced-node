99276bf3ae50a49447276c0edc2a9ad94f809df7
Thiago Turim Carvalho <tiago.carvalho@br.experian.com>
2021-09-16 07:54:29 -0300

    make FacebookLoginController use RequiredStringValidator

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -7,7 +7,8 @@ import {
 } from '@/application/helpers';
 import { FacebookAuthentication } from '@/domain/features';
 import { AccessToken } from '@/domain/models';
-import { RequiredFieldError, UnknownError } from '@/application/errors';
+import { UnknownError } from '@/application/errors';
+import { RequiredStringValidator } from '../validation';
 
 type HttpRequest = {
   token: string;
@@ -44,12 +45,7 @@ export class FacebookLoginController {
   }
 
   private validate(httpRequest: HttpRequest): Error | undefined {
-    if (
-      httpRequest.token === '' ||
-      httpRequest.token === null ||
-      httpRequest.token === undefined
-    ) {
-      return new RequiredFieldError('token');
-    }
+    const validator = new RequiredStringValidator(httpRequest.token, 'token');
+    return validator.validate();
   }
 }
