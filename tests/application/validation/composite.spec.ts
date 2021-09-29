5637974965c9421c848e0bab0ad75078a13241cf
Thiago Turim Carvalho <tiago.carvalho@br.experian.com>
2021-09-28 23:04:15 -0300

    isolate SUT creation

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,4 +1,4 @@
-import { mock } from 'jest-mock-extended';
+import { mock, MockProxy } from 'jest-mock-extended';
 
 interface Validator {
   validate(): Error | undefined;
@@ -13,13 +13,22 @@ class ValidationComposite implements Validator {
 }
 
 describe('ValidationComposite', () => {
-  it('should return undefined if all validator return undefined', () => {
+  let sut: ValidationComposite;
+  let validator1: MockProxy<Validator>;
+  let validator2: MockProxy<Validator>;
+
+  beforeAll(() => {
     const validator1 = mock<Validator>();
     validator1.validate.mockReturnValue(undefined);
     const validator2 = mock<Validator>();
     validator2.validate.mockReturnValue(undefined);
-    const validators = [validator1, validator2];
-    const sut = new ValidationComposite(validators);
+  });
+
+  beforeEach(() => {
+    sut = new ValidationComposite([validator1, validator2]);
+  });
+
+  it('should return undefined if all validator return undefined', () => {
     const error = sut.validate();
     expect(error).toBeUndefined();
   });
