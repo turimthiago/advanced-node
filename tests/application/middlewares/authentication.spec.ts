6f018e43da6a3d2388b17843d3553922ac40c715
Thiago Turim Carvalho <thiago.carvalho@dextra-sw.com>
2021-11-03 23:44:35 -0300

    ensure AuthenticationMiddleware call authorize with correct values

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,18 +1,36 @@
+import { Authorize } from '@/domain/use-cases';
 import { forbidden, HttpResponse } from '@/application/helpers';
+import { RequiredStringValidator } from '@/application/validation';
 
 type HttpRequest = { authorization: string };
 
 class AuthenticationMiddleware {
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse<Error>> {
-    return forbidden();
+  constructor(private readonly authorize: Authorize) {}
+
+  async handle({
+    authorization
+  }: HttpRequest): Promise<HttpResponse<Error> | undefined> {
+    const error = new RequiredStringValidator(
+      authorization,
+      'authorization'
+    ).validate();
+    if (error !== undefined) return forbidden();
+    await this.authorize({ token: authorization });
   }
 }
 
 describe('AuthenticationMiddleware', () => {
   let sut: AuthenticationMiddleware;
+  let authorization: string;
+  let authorize: jest.Mock;
+
+  beforeAll(() => {
+    authorize = jest.fn();
+    authorization = 'any_authorization_token';
+  });
 
   beforeEach(() => {
-    sut = new AuthenticationMiddleware();
+    sut = new AuthenticationMiddleware(authorize);
   });
 
   it('should return 403 if authorization is empty', async () => {
@@ -28,5 +46,13 @@ describe('AuthenticationMiddleware', () => {
   it('should return 403 if authorization is undefined', async () => {
     const response = await sut.handle({ authorization: undefined as any });
     expect(response).toEqual(forbidden());
+  });
+
+  it('should call authorize with correct input', async () => {
+    await sut.handle({ authorization });
+    expect(authorize).toHaveBeenLastCalledWith({
+      token: authorization
+    });
+    expect(authorize).toHaveBeenCalledTimes(1);
   });
 });
