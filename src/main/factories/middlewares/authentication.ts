772d35d83acb4b8f1760ee94515d3e7b1801c925
Thiago Turim <turimthiago@gmail.com>
2021-11-05 16:51:13 -0300

    remove anti patten middle man

--- src/main/factories/middlewares/authentication.ts
+++ src/main/factories/middlewares/authentication.ts
@@ -1,8 +1,7 @@
-import { setupAuthorize } from '@/domain/use-cases';
 import { AuthenticationMiddleware } from '@/application/middlewares';
 import { makeJwtTokenGenerator } from '@/main/factories/crypto';
 
 export const makeAuthenticationMiddleware = (): AuthenticationMiddleware => {
-  const authorize = setupAuthorize(makeJwtTokenGenerator());
-  return new AuthenticationMiddleware(authorize);
+  const jwt = makeJwtTokenGenerator();
+  return new AuthenticationMiddleware(jwt.validateToken.bind(jwt));
 };
