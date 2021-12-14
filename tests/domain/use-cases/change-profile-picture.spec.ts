8ee12258f032dc50e4de75743395ccdcaa91eca6
Thiago Turim <turimthiago@gmail.com>
2021-12-14 15:46:33 -0300

    isolate sut creation

--- tests/domain/use-cases/change-profile-picture.spec.ts
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -1,4 +1,4 @@
-import { mock } from 'jest-mock-extended';
+import { mock, MockProxy } from 'jest-mock-extended';
 
 type Setup = (
   fileStorage: UploadFile,
@@ -31,13 +31,25 @@ namespace UUIDGenerator {
 }
 
 describe('ChangeProfilePicture', () => {
-  it('should call UploadFile with correct input', async () => {
-    const uuid = 'any_unique_id';
-    const crypto = mock<UUIDGenerator>();
+  let uuid: string;
+  let crypto: MockProxy<UUIDGenerator>;
+  let file: Buffer;
+  let fileStorage: MockProxy<UploadFile>;
+  let sut: ChangeProfilePicture;
+
+  beforeAll(() => {
+    uuid = 'any_unique_id';
+    crypto = mock<UUIDGenerator>();
     crypto.uuid.mockReturnValue(uuid);
-    const file = Buffer.from('any_buffer');
-    const fileStorage = mock<UploadFile>();
-    const sut = setupChangeProfilePicture(fileStorage, crypto);
+    file = Buffer.from('any_buffer');
+    fileStorage = mock<UploadFile>();
+  });
+
+  beforeEach(() => {
+    sut = setupChangeProfilePicture(fileStorage, crypto);
+  });
+
+  it('should call UploadFile with correct input', async () => {
     await sut({ id: 'any_id', file });
     expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: uuid });
     expect(fileStorage.upload).toHaveBeenCalledTimes(1);
