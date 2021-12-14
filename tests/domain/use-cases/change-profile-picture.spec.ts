07314d4ac5b30a8684d8c1dc98f6a21fafd2faa4
Thiago Turim <turimthiago@gmail.com>
2021-12-14 15:20:47 -0300

    ensure call UploadFile with correct input

---
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -0,0 +1,31 @@
+import { mock } from 'jest-mock-extended';
+
+type Setup = (fileStorage: UploadFile) => ChangeProfilePicture;
+type Input = { id: string; file: Buffer };
+type ChangeProfilePicture = (input: Input) => Promise<void>;
+
+const setupChangeProfilePicture: Setup =
+  (fileStorage) =>
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async ({ id, file }) => {
+    await fileStorage.upload({ file, key: id });
+  };
+
+interface UploadFile {
+  upload: (input: UploadFile.Input) => Promise<void>;
+}
+
+namespace UploadFile {
+  export type Input = { file: Buffer; key: string };
+}
+
+describe('ChangeProfilePicture', () => {
+  it('should call UploadFile with correct inout', async () => {
+    const file = Buffer.from('any_buffer');
+    const fileStorage = mock<UploadFile>();
+    const sut = setupChangeProfilePicture(fileStorage);
+    await sut({ id: 'any_id', file });
+    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: 'any_id' });
+    expect(fileStorage.upload).toHaveBeenCalledTimes(1);
+  });
+});
