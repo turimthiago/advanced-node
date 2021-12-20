ebcde6c5a957bf1ec67bca1f0961bb736d0947db
Thiago Turim <turimthiago@gmail.com>
2021-12-20 16:18:59 -0300

    isolate sut creation

--- tests/infra/crypto/uui-handler.spec.ts
+++ tests/infra/crypto/uui-handler.spec.ts
@@ -11,16 +11,25 @@ class UUIDHandler implements UUIDGenerator {
 }
 
 describe('UUIDHandler', () => {
+  let key: string;
+  let sut: UUIDHandler;
+
+  beforeAll(() => {
+    mocked(v4).mockReturnValue('any_uuid');
+    key = 'any_key';
+  });
+
+  beforeEach(() => {
+    sut = new UUIDHandler();
+  });
+
   it('should call uuid.v4', () => {
-    const sut = new UUIDHandler();
-    sut.uuid({ key: 'any_key' });
+    sut.uuid({ key });
     expect(v4).toHaveBeenCalledTimes(1);
   });
 
   it('should return correct uuid', () => {
-    mocked(v4).mockReturnValueOnce('any_uuid');
-    const sut = new UUIDHandler();
-    const uuid = sut.uuid({ key: 'any_key' });
+    const uuid = sut.uuid({ key });
     expect(uuid).toBe('any_key_any_uuid');
   });
 });
