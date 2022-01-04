abc31c1c051ad625c5c889e5c5f2bf9f358fa6da
Thiago Turim <turimthiago@gmail.com>
2022-01-04 17:18:50 -0300

    move AwsS3FileStorage to its own file

--- src/infra/gateways/index.ts
+++ src/infra/gateways/index.ts
@@ -3,4 +3,5 @@ export * from '../gateways/jwt-token-handler';
 export * from './uuid-handler';
 export * from './unique-uuid';
 export * from './client';
-export * from '../gateways/axios-client';
+export * from './axios-client';
+export * from './aws-s3-file-storeage';
