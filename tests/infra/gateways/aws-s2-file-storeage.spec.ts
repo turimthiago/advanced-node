98d8327d7f337f5b97b87d0110b43daafb01fbd8
Thiago Turim <turimthiago@gmail.com>
2022-01-04 16:56:22 -0300

    ensure Aws3FileStoreage calls putObject with correct input

--- tests/infra/gateways/aws-s2-file-storeage.spec.ts
+++ tests/infra/gateways/aws-s2-file-storeage.spec.ts
@@ -1,33 +1,65 @@
-import { config } from 'aws-sdk';
+import { UploadFile } from '@/domain/contracts/gateways';
+import { config, S3 } from 'aws-sdk';
+import { mocked } from 'ts-jest/utils';
 
 jest.mock('aws-sdk');
 
 class AwsS3FileStorage {
   constructor(
-    private readonly accessKey: string,
-    private readonly secret: string
+    accessKey: string,
+    secret: string,
+    private readonly bucket: string
   ) {
     config.update({
       credentials: {
-        accessKeyId: this.accessKey,
-        secretAccessKey: this.secret
+        accessKeyId: accessKey,
+        secretAccessKey: secret
       }
     });
+  }
+
+  async upload({ key, file }: UploadFile.Input): Promise<void> {
+    const s3 = new S3();
+    await s3
+      .putObject({
+        Bucket: this.bucket,
+        Key: key,
+        Body: file,
+        ACL: 'public-read'
+      })
+      .promise();
   }
 }
 
 describe('AwsS3FileStorage', () => {
   let accessKey: string;
   let secret: string;
+  let bucket: string;
+  let key: string;
+  let file: Buffer;
+  let putObjectPromiseSpy: jest.Mock;
+  let putObjectSpy: jest.Mock;
   let sut: AwsS3FileStorage;
 
   beforeAll(() => {
     accessKey = 'any_access_key';
     secret = 'any_secret';
+    bucket = 'any_bucket';
+    key = 'any_key';
+    file = Buffer.from('any_buffer');
+    putObjectPromiseSpy = jest.fn();
+    putObjectSpy = jest
+      .fn()
+      .mockImplementation(() => ({ promise: putObjectPromiseSpy }));
+    mocked(S3).mockImplementation(
+      jest.fn().mockImplementation(() => ({
+        putObject: putObjectSpy
+      }))
+    );
   });
 
   beforeEach(() => {
-    sut = new AwsS3FileStorage(accessKey, secret);
+    sut = new AwsS3FileStorage(accessKey, secret, bucket);
   });
 
   it('should config aws credentials on creation', () => {
@@ -39,5 +71,17 @@ describe('AwsS3FileStorage', () => {
       }
     });
     expect(config.update).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call put object with correct input', async () => {
+    await sut.upload({ key, file });
+    expect(putObjectSpy).toHaveBeenCalledWith({
+      Bucket: bucket,
+      Key: key,
+      Body: file,
+      ACL: 'public-read'
+    });
+    expect(putObjectSpy).toHaveBeenCalledTimes(1);
+    expect(putObjectPromiseSpy).toHaveBeenCalledTimes(1);
   });
 });
