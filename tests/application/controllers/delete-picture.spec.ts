f3864d2f98ab3cd692347173dca1e45388ba9a40
Thiago Turim <turimthiago@gmail.com>
2022-01-05 14:50:53 -0300

    ensure DeletePictureController call ChangeProfilePicture with correct input

---
+++ tests/application/controllers/delete-picture.spec.ts
@@ -0,0 +1,20 @@
+import { ChangeProfilePicture } from '@/domain/use-cases';
+
+type HttpRequest = { userId: string };
+
+class DeletePictureController {
+  constructor(private readonly changeProfilePicture: ChangeProfilePicture) {}
+
+  async handle({ userId }: HttpRequest): Promise<void> {
+    this.changeProfilePicture({ id: userId });
+  }
+}
+describe('DeletePictureController', () => {
+  it('should call ChangeProfilePicture with correct input', async () => {
+    const changeProfilePicture = jest.fn();
+    const sut = new DeletePictureController(changeProfilePicture);
+    await sut.handle({ userId: 'any_user_id' });
+    expect(changeProfilePicture).toHaveBeenCalledWith({ id: 'any_user_id' });
+    expect(changeProfilePicture).toHaveBeenCalledTimes(1);
+  });
+});
