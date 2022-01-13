cdc77fcd6322de85e1b9d5f78e87d0c2d01cb987
Thiago Turim <turimthiago@gmail.com>
2022-01-13 16:06:30 -0300

    move code from tests to src

--- tests/application/validation/allowed-mime-types.spec.ts
+++ tests/application/validation/allowed-mime-types.spec.ts
@@ -1,26 +1,5 @@
-import { String } from 'aws-sdk/clients/appstream';
 import { InvalidMimeTypeError } from '@/application/errors';
-
-type Extension = 'png' | 'jpg';
-export class AllowedMimeTypes {
-  constructor(
-    private readonly allowed: Extension[],
-    private readonly mimeType: String
-  ) {}
-
-  validate(): Error | undefined {
-    if (!this.isPng() && !this.isJpg())
-      return new InvalidMimeTypeError(this.allowed);
-  }
-
-  private isPng(): boolean {
-    return this.allowed.includes('png') && this.mimeType === 'image/png';
-  }
-
-  private isJpg(): boolean {
-    return this.allowed.includes('jpg') && /image\/jpe?g/.test(this.mimeType);
-  }
-}
+import { AllowedMimeTypes } from '@/application/validation';
 
 describe('AllowedMimeTypes', () => {
   it('should return InvalidMimeTypeError if value is invalid', () => {
