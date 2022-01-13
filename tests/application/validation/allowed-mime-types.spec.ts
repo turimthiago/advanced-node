334846fe8c676aa35cef950f6ad943f798357714
Thiago Turim <turimthiago@gmail.com>
2022-01-13 15:28:10 -0300

    ensure AllowedMimeTypes returns undefined if value is valid

--- tests/application/validation/allowed-mime-types.spec.ts
+++ tests/application/validation/allowed-mime-types.spec.ts
@@ -2,15 +2,15 @@ import { String } from 'aws-sdk/clients/appstream';
 import { InvalidMimeTypeError } from '@/application/errors';
 
 type Extension = 'png' | 'jpg';
-
 export class AllowedMimeTypes {
   constructor(
     private readonly allowed: Extension[],
     private readonly mimeType: String
   ) {}
 
-  validate(): Error {
-    return new InvalidMimeTypeError(this.allowed);
+  validate(): Error | undefined {
+    if (this.allowed.includes('png') && this.mimeType !== 'image/png')
+      return new InvalidMimeTypeError(this.allowed);
   }
 }
 
@@ -19,5 +19,11 @@ describe('AllowedMimeTypes', () => {
     const sut = new AllowedMimeTypes(['png'], 'image/jpg');
     const error = sut.validate();
     expect(error).toEqual(new InvalidMimeTypeError(['png']));
+  });
+
+  it('should return undefined if value is valid', () => {
+    const sut = new AllowedMimeTypes(['png'], 'image/png');
+    const error = sut.validate();
+    expect(error).toBeUndefined();
   });
 });
