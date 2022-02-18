0799f0b7da393191e16a08550e9a5ff7e9ba7bc6
Thiago Turim <turimthiago@gmail.com>
2022-02-18 13:44:50 -0300

    change server error to receive unknown instaead of Error

--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -24,9 +24,9 @@ export const unauthorized = (): HttpResponse<Error> => ({
   data: new UnauthorizedError()
 });
 
-export const serverError = (error: Error): HttpResponse<Error> => ({
+export const serverError = (error: unknown): HttpResponse<Error> => ({
   statusCode: 500,
-  data: new ServerError(error)
+  data: new ServerError(error instanceof Error ? error : undefined)
 });
 
 export const forbidden = (): HttpResponse<Error> => ({
--- tests/application/controllers/controller.spec.ts
+++ tests/application/controllers/controller.spec.ts
@@ -1,7 +1,7 @@
-import { ServerError } from '@/application/errors';
-import { ValidationComposite } from '@/application/validation';
-import { HttpResponse } from '@/application/helpers';
 import { Controller } from '@/application/controllers';
+import { ServerError } from '@/application/errors';
+import { HttpResponse } from '@/application/helpers';
+import { ValidationComposite } from '@/application/validation';
 
 import { mocked } from 'ts-jest/utils';
 
@@ -10,8 +10,9 @@ jest.mock('@/application/validation/composite');
 class ControllerStub extends Controller {
   result: HttpResponse = {
     statusCode: 200,
-    data: 'any_date'
+    data: 'any_data'
   };
+
   async perform(httpRequest: any): Promise<HttpResponse> {
     return this.result;
   }
@@ -27,11 +28,11 @@ describe('Controller', () => {
   it('should return 400 if validation fails', async () => {
     const error = new Error('validation_error');
     const ValidationCompositeSpy = jest.fn().mockImplementationOnce(() => ({
-      validate: jest.fn().mockReturnValue(error)
+      validate: jest.fn().mockReturnValueOnce(error)
     }));
     mocked(ValidationComposite).mockImplementationOnce(ValidationCompositeSpy);
     const httpResponse = await sut.handle('any_value');
-    expect(ValidationCompositeSpy).toHaveBeenCalledWith([]);
+    expect(ValidationComposite).toHaveBeenCalledWith([]);
     expect(httpResponse).toEqual({
       statusCode: 400,
       data: error
@@ -39,6 +40,16 @@ describe('Controller', () => {
   });
 
   it('should return 500 if perform throws', async () => {
+    const error = new Error('perform_error');
+    jest.spyOn(sut, 'perform').mockRejectedValueOnce(error);
+    const httpResponse = await sut.handle('any_value');
+    expect(httpResponse).toEqual({
+      statusCode: 500,
+      data: new ServerError(error)
+    });
+  });
+
+  it('should return 500 if perform throws a non error object', async () => {
     jest.spyOn(sut, 'perform').mockRejectedValueOnce('perform_error');
     const httpResponse = await sut.handle('any_value');
     expect(httpResponse).toEqual({
